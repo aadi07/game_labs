ca8dc9fd9000c9d145f43cacbba69a41c757fa85
Aadi <aadi@Aadis-MacBook.local>
2016-11-17 18:10:34 +0530

    First commit

---
+++ ftoc.py
@@ -0,0 +1,3 @@
+fahrenheit = input("Enter temperature in Fahrenheit:")
+celsius = (float(fahrenheit) - 32.0) * 5.0/9.0
+print("The temperature in Celsius :", celsius)
---
+++ trapezoid.py
@@ -0,0 +1,5 @@
+h = input("Enter the height of the trapezoid :")
+x1 = input("Enter the length of the bottom base :")
+x2 = input("Enter the length of the top base :")
+A = 1.0/2.0 * (float(x1) + float(x2)) * float(h)
+print("The area is :", A)
